a8e9ed67bc506394cf83e3b95d97d414e026ba0c
ChistyakovDanil <dsa3142@gmail.com>
2022-12-18 14:06:29 +0200

    deleteSpace

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
-import { useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-import './App.css'
-import TodoForm from './components/Todos/TodoForm'
-import TodoList from './components/Todos/TodoList'
-import TodosActions from './components/Todos/TodosActions'
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
+import TodoForm from './components/Todos/TodoForm';
+import TodoList from './components/Todos/TodoList';
+import TodosActions from './components/Todos/TodosActions';
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const addTodoHandler = (text) => {
-    const newTodo = {
-      text,
-      isCompleted: false,
-      id: uuidv4(),
+    if (text.match(/^[ ]+$/)) {
+      text = '';
+    } else {
+      const newTodo = {
+        text,
+        isCompleted: false,
+        id: uuidv4(),
+      };
+      setTodos([...todos, newTodo]);
     }
-    setTodos([...todos, newTodo])
-  }
+  };
 
   const deleteTodoHandler = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id))
-  }
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   const toggleTodoHandler = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id
-          ? { ...todo, isCompleted: !todo.isCompleted }
-          : { ...todo }
-      )
-    )
-  }
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : { ...todo })));
+  };
 
   const resetTodosHandler = () => {
-    setTodos([])
-  }
+    setTodos([]);
+  };
 
   const deleteCompletedTodosHandler = () => {
-    setTodos(todos.filter((todo) => !todo.isCompleted))
-  }
+    setTodos(todos.filter((todo) => !todo.isCompleted));
+  };
 
-  const completedTodosCount = todos.filter((todo) => todo.isCompleted).length
+  const completedTodosCount = todos.filter((todo) => todo.isCompleted).length;
 
   return (
     <div className="App">
@@ -52,18 +50,12 @@ function App() {
           deleteTodo={deleteCompletedTodosHandler}
         />
       )}
-      <TodoList
-        todos={todos}
-        deleteTodo={deleteTodoHandler}
-        toggleTodo={toggleTodoHandler}
-      />
+      <TodoList todos={todos} deleteTodo={deleteTodoHandler} toggleTodo={toggleTodoHandler} />
       {completedTodosCount > 0 && (
-        <h2>{`You have completed ${completedTodosCount} ${
-          completedTodosCount > 1 ? 'todos' : 'todo'
-        }`}</h2>
+        <h2>{`You have completed ${completedTodosCount} ${completedTodosCount > 1 ? 'todos' : 'todo'}`}</h2>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,27 +1,23 @@
-import { useState } from 'react'
-import styles from './TodoForm.module.css'
+import { useState } from 'react';
+import styles from './TodoForm.module.css';
 
 function TodoForm({ addTodo }) {
-  const [text, setText] = useState('')
+  const [text, setText] = useState('');
 
   function onSubmitHandler(event) {
-    event.preventDefault()
-    addTodo(text)
-    setText('')
+    event.preventDefault();
+    addTodo(text);
+    setText('');
   }
 
   return (
     <div className={styles.todoFormContainer}>
       <form onSubmit={onSubmitHandler}>
-        <input
-          placeholder="Enter new todo"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
+        <input placeholder="Enter new todo" value={text} onChange={(e) => setText(e.target.value)} required />
         <button type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default TodoForm
+export default TodoForm;
